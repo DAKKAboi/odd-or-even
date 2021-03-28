f086a68e0db5ff2c5446a93a409eec6df3a713f8
DAKKAboi <66794963+DAKKAboi@users.noreply.github.com>
2021-03-28 15:19:08 +0100

    Add files via upload

---
+++ OddOrEven/OddOrEven.cpp
@@ -0,0 +1,44 @@
+// OddOrEven.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+bool isEven(int num)
+{
+    return !(num % 2);
+}
+
+int intInput()
+{
+    std::cout << "Enter an Intager:\t";
+    
+    int userInt{};
+    std::cin >> userInt;
+    return userInt;
+}
+
+void OEOutput(int num)
+{
+    bool even{ isEven(num) };
+    
+    if (even)
+        std::cout << num << " is even";
+    else
+        std::cout << num << " is odd";
+}
+
+int main()
+{
+    OEOutput(intInput());
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
